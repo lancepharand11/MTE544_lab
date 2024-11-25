2b6ca58b1ca74b169879bbb19d11ab3fc24de953
logan-hartford <91397886+logan-hartford@users.noreply.github.com>
2024-11-25 10:38:46 -0500

    did most of the planner stuff

--- planner.py
+++ planner.py
@@ -31,7 +31,7 @@ class planner:
         # TODO PART 5 Create the cost-map, the laser_sig is 
         # the standard deviation for the gausiian for which
         # the mean is located on the occupant grid. 
-        self.m_utilites=mapManipulator(laser_sig=...)
+        self.m_utilites=mapManipulator(laser_sig=0.01) # Not sure how we are supposed to determine this value
             
         self.costMap=self.m_utilites.make_likelihood_field()
         
@@ -47,14 +47,12 @@ class planner:
         startPose=self.m_utilites.position_2_cell(startPoseCart)
         endPose=self.m_utilites.position_2_cell(endPoseCart)
         
-        # TODO PART 5 convert the cell pixels into the cartesian coordinates
-        
-        Path = list(map(...))
+        # DONE PART 5 convert the cell pixels into the cartesian coordinates
+        path_cells = search(self.costMap, startPose, endPose, euclidean_dist=True)
+        Path = list(map(self.m_utilites.cell_2_position, path_cells))
 
-
-
-        # TODO PART 5 return the path as list of [x,y]
-        return ...
+        # DONE PART 5 return the path as list of [x,y]
+        return Path
 
 
 
